5982c43f82f125510ce278d0927dbc6cd25d7167
rasol_fucker <mohmadrsol81@gmail.com>
2016-06-08 13:51:50 +0430

    Update telegoldbot.lua

--- bot/telegoldbot.lua
+++ bot/telegoldbot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "webshot",
     "welcome"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {101564409},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[telegold Bot V3
