4b9c48a08614f02ab623a5a62da4f81ade283fe7
rasol_fucker <mohmadrsol81@gmail.com>
2016-06-08 20:03:13 +0430

    Create telegold.lua

---
+++ plugins/telegold.lua
@@ -0,0 +1,36 @@
+do
+
+function run(msg, matches)
+  return [[telegold Bot V3
+    An Advanced Anti Spam Bot Forked On Telegold
+    
+   sudo: @fucker_rasol_fucker
+   
+   chanel: @fucker_rasol
+    
+    #Open Source
+    https://github.com/rasol-fucker/tele-gold]]
+end
+return {
+  description = ".", 
+  usage = "use telegold command",
+  patterns = {
+    "^/telegold$",
+    "^!telegold$",
+    "^%telegold$",
+    "^$telegold$",
+   "^#telegold$",
+   "^#telegold",
+   "^/telegold$",
+   "^#telegold$",
+   "^/gold$",
+   "^!gold$",
+   "^%gold$",
+   "^$gold$",
+   "^/gold$",
+    "^#gold$",
+
+  },
+  run = run
+}
+end
